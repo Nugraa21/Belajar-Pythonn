469c72e59bd08c2c33c75e964f1a24c0769ad31c
Nugra21 <ludang400060@gmail.com>
2025-10-30 11:30:26 +0700

    -

---
+++ python/variable.py
@@ -0,0 +1,10 @@
+nama = "Nugra"
+umur = 20
+tinggi = 175.5
+
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Tipe data nama:", type(nama))
+print("Tipe data umur:", type(umur))    
